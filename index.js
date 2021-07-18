de992483f865916f185b0c0d3dd859abf36f5541
Evan C. Navarro <EvanCNavarro@gmail.com>
2021-07-17 21:41:05 -0700

    Preliminary Work
    Created Buttons
    Figured out Timer
    Set Button Values
    Disabled Buttons
    
    Co-Authored-By: Tevin Rose <23547919+Blarsu@users.noreply.github.com>
    Co-Authored-By: Richard M <richard.morand11@gmail.com>
    Co-Authored-By: danteedmonson <74576480+danteedmonson@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,39 +1,98 @@
-let timer;
 
-function runTimer() {
-  if(timer !== -1) {
-    if (timer < 10) {
-      document.querySelector("#timer").innerHTML = "00:0" + timer--;
-    }
-    else {
-      document.querySelector("#timer").innerHTML = "00:" + timer--;
-    }
-  }
-  else {
-    stopTimer();
-  }
+const timerSelected = {
+    // minutes to milliseconds
+    1: 60000,
+    5: 300000,
+    10: 600000,
+    20: 1200000,
 };
 
-function startTimer () {
-  timer = 11;
-  let timeReset = setInterval(runTimer, 100);
-};
+let start;
+let userChoice;
 
-function stopTimer() {
-  document.querySelector("#timer").innerText = "00:00";
-  clearInterval(timeReset);
-};
+function disableButtons() {
+    document.querySelector("#start").disabled = true;
 
-window.onload = stopTimer;
+    document.querySelector("#one").checked = false;
+    document.querySelector("#five").checked = false;
+    document.querySelector("#ten").checked = true;
+    document.querySelector("#twenty").checked = false;
+}
 
-var start = Date.now();
-console.log(start);
-setInterval(function() {
-  var delta = Date.now() - start; // milliseconds elapsed since start
-  console.log(delta);
-  output(Math.floor(delta / 1000)); // in seconds
-  // alternatively just show wall clock time:
-  output(new Date().toUTCString());
-}, 1000); // update about every second
 
-console.log(start);
+function setTimer(options) {
+    document.querySelector("#start").disabled = false;
+    document.querySelector("#timer").innerHTML = options + ":00 minutes";
+    userChoice = options;
+}
+
+// function startTimer() {
+//     start = Date.now() + 1000;
+//     setTimeout(initiate, 1000);
+// }
+
+// function initiate() {
+    
+//     let currentTime = Date.now();
+//     // 510ms
+
+//     let dateDifference = currentTime - start;
+//     // 10ms
+
+//     // THEY CHOSE 5 MINUTES
+//     let timeRemaining = timerSelected[userChoice] - dateDifference;
+//     // 30,000 - 10 == 29,990ms
+
+//     // show the time going down
+//     // disable the buttons
+//     // show pause and stop
+
+//     // 300000 / 1000ms = 300 seconds
+//     let timeShown = timeRemaining / 1000; // 280 seconds
+//     let timeShownMinutes = Math.floor(timeShown / 60); // 280 / 60 seconds = 4
+//     let timeShownSeconds = Math.floor(timeShown % 60); // 280 % 60 seconds = 40 seconds
+//     document.querySelector("#timer").innerHTML =
+//         timeShownMinutes + ":" + timeShownSeconds + " minutes";
+//     setTimeout(initiate, Math.max(0, 1000 - dateDifference));
+
+// }
+
+function startTimer() {
+    document.querySelector("#start").hidden = true;
+    document.querySelector("#pause").hidden = false;
+    document.querySelector("#stop").hidden = false;
+
+    // figure out a way to simplify using the whole class
+    document.querySelector("#one").disabled = true;
+    document.querySelector("#five").disabled = true;
+    document.querySelector("#ten").disabled = true;
+    document.querySelector("#twenty").disabled = true;
+
+    start = Date.now() + 1000;
+    setInterval(functionality, 1000);
+}
+
+function functionality() {
+    
+    let currentTime = Date.now();
+    // 510ms
+
+    let dateDifference = currentTime - start;
+    // 10ms
+
+    // THEY CHOSE 5 MINUTES
+    let timeRemaining = timerSelected[userChoice] - dateDifference;
+    // 30,000 - 10 == 29,990ms
+
+    // show the time going down
+    // disable the buttons
+    // show pause and stop
+
+    // 300000 / 1000ms = 300 seconds
+    let timeShown = timeRemaining / 1000; // 280 seconds
+    let timeShownMinutes = Math.floor(timeShown / 60); // 280 / 60 seconds = 4
+    let timeShownSeconds = Math.floor(timeShown % 60); // 280 % 60 seconds = 40 seconds
+    document.querySelector("#timer").innerHTML =
+        timeShownMinutes + ":" + timeShownSeconds + " minutes";
+
+}
