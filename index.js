b0a0c7e7764c667c4f2b590649d1f26a891e8800
Evan C. Navarro <EvanCNavarro@gmail.com>
2021-07-19 18:11:03 -0700

    15 minutes
    - added millisecond for 15 min
    - adjusted hidden/show values for buttons
    
    Co-Authored-By: Tevin Rose <23547919+Blarsu@users.noreply.github.com>
    Co-Authored-By: danteedmonson <74576480+danteedmonson@users.noreply.github.com>
    Co-Authored-By: Richard M <richard.morand11@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 const timerSelected = {
     // minutes to milliseconds
-    1: 60000,
+    1: 60000,   // 1 minute == 60seconds * 1000 = 60,000
     5: 300000,
     10: 600000,
+    15: 900000,
     20: 1200000,
 };
 
@@ -13,10 +14,10 @@ let userChoice;
 function disableButtons() {
     document.querySelector("#start").disabled = true;
 
-    document.querySelector("#one").checked = false;
-    document.querySelector("#five").checked = false;
-    document.querySelector("#ten").checked = true;
-    document.querySelector("#twenty").checked = false;
+    // document.querySelector("#one").checked = false;
+    // document.querySelector("#five").checked = false;
+    // document.querySelector("#ten").checked = true;
+    // document.querySelector("#twenty").checked = false;
 }
 
 
@@ -58,9 +59,10 @@ function setTimer(options) {
 // }
 
 function startTimer() {
-    document.querySelector("#start").hidden = true;
+    document.querySelector("#start").disabled = true;
     document.querySelector("#pause").hidden = false;
     document.querySelector("#stop").hidden = false;
+    document.querySelector("#add30").hidden = false;
 
     // figure out a way to simplify using the whole class
     document.querySelector("#one").disabled = true;
@@ -68,30 +70,40 @@ function startTimer() {
     document.querySelector("#ten").disabled = true;
     document.querySelector("#twenty").disabled = true;
 
+
     start = Date.now() + 1000;
+    // 5000ms + 1000ms
+    // start = 6000ms
+
     setInterval(functionality, 1000);
 }
 
 function functionality() {
     
     let currentTime = Date.now();
-    // 510ms
+    // 6000 ms
 
     let dateDifference = currentTime - start;
-    // 10ms
+    // miniscule value
+    // essentially === 6000 - 6000 = 0
 
-    // THEY CHOSE 5 MINUTES
     let timeRemaining = timerSelected[userChoice] - dateDifference;
-    // 30,000 - 10 == 29,990ms
+    // 59,999 / 1000 = 59 seconds
 
-    // show the time going down
-    // disable the buttons
-    // show pause and stop
 
-    // 300000 / 1000ms = 300 seconds
-    let timeShown = timeRemaining / 1000; // 280 seconds
-    let timeShownMinutes = Math.floor(timeShown / 60); // 280 / 60 seconds = 4
-    let timeShownSeconds = Math.floor(timeShown % 60); // 280 % 60 seconds = 40 seconds
+    // what happens is:
+    //      we select 1 minute
+    //      we click start
+    //      the timer goes from showing 1:00, to actually calculating
+    //      the calculation ALSO shows 1:00
+
+    // 1 minute
+    // 59 seconds
+    // 58
+
+    let timeShown = timeRemaining / 1000;
+    let timeShownMinutes = Math.floor(timeShown / 60);
+    let timeShownSeconds = Math.floor(timeShown % 60); 
     document.querySelector("#timer").innerHTML =
         timeShownMinutes + ":" + timeShownSeconds + " minutes";
 
